101584a6ff0affa09cebd421d8b756afdd54e1be
Sundar Karthik S <sundar1007karthik@gmail.com>
2025-03-23 23:30:35 +0530

    initialize components and styles for home, login, settings, not-found, city-weather, map-view, and city-search pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './common/menu-bar/menu-bar.component';
+import { LoginComponent } from './common/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { CityWeatherComponent } from './pages/city-weather/city-weather.component';
+import { CitySearchComponent } from './components/city-search/city-search.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { MapViewComponent } from './components/map-view/map-view.component';
+import { TemperaturePipe } from './pipes/temperature.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    LoginComponent,
+    HomeComponent,
+    NotFoundComponent,
+    SettingsComponent,
+    CityWeatherComponent,
+    CitySearchComponent,
+    WeatherCardComponent,
+    MapViewComponent,
+    TemperaturePipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/city-search/city-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-search',
+  templateUrl: './city-search.component.html',
+  styleUrl: './city-search.component.css'
+})
+export class CitySearchComponent {
+
+}
